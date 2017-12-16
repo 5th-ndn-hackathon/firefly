2b11e1f855bda8911d615a7c32ffee6f8b2eb5e4
peetonn <gpeetonn@gmail.com>
2017-12-16 15:34:41 -0800

    firefly sync v1

---
+++ firefly-sync.js
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2017-2017 Regents of the University of California.
+ * @author: Peter Gusev <peter@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+var FireflySync = function FireflySync(firestoreDb, syncDoc, applicationPrefix, onReceivedSyncState){
+	this.firestoreDb = firestoreDb;
+	this.onReceivedSyncState = onReceivedSyncState;
+	this.applicationDataPrefixUri = applicationPrefix;
+	this.syncDoc = syncDoc;
+	this.syncData = {};
+	this.mySeqNo = -1;
+	var self = this;
+
+	this.firestoreDb.doc(this.syncDoc).onSnapshot(function(snapshot){
+		if (!snapshot.exists)
+		{
+			console.log('> firefly-sync: sync doc does not exist.');
+  			// setup empty document
+  			self.firestoreDb.doc(self.syncDoc).set({})
+  			.then(function(){
+  				console.log('> firefly-sync: new sync doc created');
+  			})
+  			.catch(function(error){
+  				console.error('> firefly-sync: error creating sync doc: ', error);
+  			});
+		}
+		else
+		{
+			// console.log('> firefly-sync: received doc snapshot: ', snapshot.data());
+
+			var delta = {}
+			var syncData = snapshot.data();
+			for (var key in syncData)
+			{
+				var decodedKey = decodeURIComponent(key);
+				var newValue = !(decodedKey in self.syncData) && decodedKey != self.applicationDataPrefixUri;
+				if (!newValue) newValue = (syncData[key] > self.syncData[decodedKey]);
+				if (newValue) delta[decodedKey] = syncData[key];
+				// update our seq no if needed
+				if (decodedKey == self.applicationDataPrefixUri && self.mySeqNo < syncData[key])
+					self.mySeqNo = syncData[key];
+			}
+
+			if (Object.keys(delta).length)
+				self.onReceivedSyncState(delta);
+			else
+				console.log('> firefly-sync: no sync updates');
+		}
+	}, 
+	function(err) {
+    	console.log('> firefly-sync: encountered error while getting updates: ${err}');
+	});
+};
+
+FireflySync.prototype.publishNextSequenceNo = function(){
+	this.mySeqNo++;
+	var prefix = encodeURIComponent(this.applicationDataPrefixUri);
+	var self = this;
+
+	this.firestoreDb.doc(this.syncDoc).update({
+		[prefix] : self.mySeqNo
+	})
+	.then(function(){
+		console.log("> firefly-sync: updated sync doc with new seq no ", self.mySeqNo);
+	})
+	.catch(function(error){
+		console.error('> firefly-sync: error updating sync doc: ', error);
+	});
+
+	return this.mySeqNo;
+}
